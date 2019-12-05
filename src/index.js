2a3ee2fd3b734c7293d4ce27d8efff34caa2112d
Andres Ramirez <andresrm88@gmail.com>
2019-12-05 14:40:27 +0100

    Added title

--- src/index.js
+++ src/index.js
@@ -36,6 +36,20 @@ ReactDOM.createRoot(document.getElementById('root'))
 
 if('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js');
+    navigator.serviceWorker.register('./sw.js').then((registration) => {
+      console.log(`El service worker SW se ha registrado correctamente ${registration.scope}`)
+
+      registration.addEventListener("updatefound", () => {
+        const worker = registration.installing;
+
+        worker.addEventListener('statechange', () => {
+          if(worker.state == "installed") {
+            // Do something here when the worker is installed
+          }
+        })
+      })
+    }, (error) => {
+      console.error(`El registro de SW ha fallado: ${err}`)
+    });
   })
 }
